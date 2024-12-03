17b4752845a0a81f5fbc8f87ccad0259fdb3676b
Nzanzu Lwanzo <160411106+Nzanzu-Lwanzo@users.noreply.github.com>
2024-12-03 15:45:00 +0200

    Remove restriction on /register path.Even if there's a user in the localStorage, we can still go there and create another account.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,10 @@ import Room from "./pages/Room";
 import Call from "./pages/Call";
 import Register from "./pages/Register";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { useAppContext } from "./contexts/AppContext";
 import { SocketContextProvider } from "./contexts/SocketContext";
 import { SnackbarProvider } from "notistack";
 
 function App() {
-  const { auth } = useAppContext();
-
   return (
     <>
       <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
@@ -19,7 +16,7 @@ function App() {
               <Route path="/" element={<Call />} />
               <Route
                 path="/register"
-                element={auth ? <Navigate to="/" /> : <Register />}
+                element={<Register />}
               />
             </Routes>
           </BrowserRouter>
